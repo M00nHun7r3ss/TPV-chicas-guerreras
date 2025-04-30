f4c740fb9dcaa97ce3c0ec234037ab9a7660520a
M00nHun7r3ss <its.denisa2004@gmail.com>
2025-04-30 10:36:36 +0200

    Commiteo pero ya de ya
    Gracias gente de cpp por esto
    
    Co-Authored-By: acostacion <125396552+acostacion@users.noreply.github.com>

--- Practicas/P3 - LittleWolf/src/client.bat
+++
@@ -1,9 +0,0 @@
-:: cambiar a la carpeta donde está la carpeta resources
-
-c:
-cd c:\Users\Denisa\Desktop\Universidad\Segundo_2\TPV2\TPV-chicas-guerreras\Prácticas\P3 - LittleWolf
- 
-:: ejecutar un cliente
-
-bin\TPV2Debug.exe client localhost 2000
-
--- Practicas/P3 - LittleWolf/src/server.bat
+++
@@ -1,8 +0,0 @@
-:: cambiar a la carpeta donde está la carpeta resources
-
-c:
-cd c:\Users\Denisa\Desktop\Universidad\Segundo_2\TPV2\TPV-chicas-guerreras\Prácticas\P3 - LittleWolf
- 
-:: ejecutar el servidor en el puerto 2000
-
-bin\TPV2Debug.exe server 2000
